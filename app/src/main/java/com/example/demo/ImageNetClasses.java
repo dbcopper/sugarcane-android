cb1a67a746ffd5f5a5fc41a93e3e89818cdc3f46
DBcopper <314539763@qq.com>
2023-04-05 16:22:42 +0800

    first pull

---
+++ app/src/main/java/com/example/demo/ImageNetClasses.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class ImageNetClasses {
+    public static String[] IMAGENET_CLASSES = new String[]{
+            "Brown spot",
+            "Brown stripe",
+            "Downy mildew",
+            "Health",
+            "Red rot",
+            "Ring spot",
+            "Yellow spot",
+    };
+}
